2ef50619c0e333fea0ca61467ede033dbacf795f
Fumiaki MATSUSHIMA <mtsmfm@gmail.com>
2016-09-05 14:54:53 +0900

    $ bundle gem fluent-plugin-google-sheets

---
+++ lib/fluent/plugin/google/sheets/version.rb
@@ -0,0 +1,9 @@
+module Fluent
+  module Plugin
+    module Google
+      module Sheets
+        VERSION = "0.1.0"
+      end
+    end
+  end
+end
---
+++ test/fluent/plugin/google/sheets_test.rb
@@ -0,0 +1,11 @@
+require 'test_helper'
+
+class Fluent::Plugin::Google::SheetsTest < Minitest::Test
+  def test_that_it_has_a_version_number
+    refute_nil ::Fluent::Plugin::Google::Sheets::VERSION
+  end
+
+  def test_it_does_something_useful
+    assert false
+  end
+end
